44780419c98c25d001324b9991f5349bac79cf34
sjodie1157 <sjodie1157@gmail.com>
2023-10-10 12:23:32 +0200

    added code

--- script/code.js
+++ script/code.js
@@ -1,1 +1,52 @@
-console.log("my name is Jeff")
+let firstName = 'Jeff'
+console.log("my name is " + firstName)
+console.log(firstName + " has 3 brothers")
+
+console.log(`my name is ${firstName}`)
+console.log(`${firstName} has 3 brothers`)
+
+//single line comment
+
+/* 
+long discription
+*/
+
+//let, or const
+// let isMale = true
+// let subjects = ['HTML 5', 'CSS 3', 'Javascript']
+// console.log(subjects);
+
+// let person = {
+//     name: 'Jodie',
+//     surname: 'Smith',
+//     age: '22'
+// }
+// console.log(person);
+
+// let data = true
+// console.log(data);
+// console.log(typeof data);
+// console.log(data[0]);
+
+
+// let number = [1, 2, 5]
+
+// console.log(number[2]);
+// console.table(number);
+// console.error("lightskinned mayo");
+
+let numb1 = "2"
+let numb2 = 2
+console.log(`sum of ${numb1} and ${numb2} is ${numb1 + numb2}`)
+console.log(`difference of ${numb1} and ${numb2} is ${numb1 - numb2}`)
+console.log(`product of ${numb1} and ${numb2} is ${numb1 * numb2}`)
+console.log(`division of ${numb2}/${numb1} = ${numb1/numb2}`);
+console.log(`${numb2}**2 ${numb2 ** 2}`);
+console.log(`what is the remainder of 12/2 = ${12%2}`);
+
+numb1 += numb2
+console.log(numb1);
+
+//comparison 
+
+console.log(numb1 == numb2);
